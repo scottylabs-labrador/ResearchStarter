2cd46f84bfd7aea4949198bd1662c92a169bff0b
Jennifer Zhu <jzhucs128@gmail.com>
2024-10-16 13:55:13 -0400

    Added Custom 404 Not Found Page

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,17 @@ import {
 } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import FilterPage from "./pages/FilterPage";
+import DashboardPage from "./pages/DashboardPage";
+import SavedPage from "./pages/SavedPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<FilterPage />} />
+      <Route path="/dashboard" element={<DashboardPage />} />
+      <Route path="/saved" element={<SavedPage />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Route>
   )
 );
