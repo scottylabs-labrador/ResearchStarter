9bcdca72416174fca79549924d7ae54d22387206
Jennifer Zhu <jzhucs128@gmail.com>
2024-10-26 10:09:25 -0400

    Edited Spinner

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -4,7 +4,12 @@ const Spinner = (loading) => {
     margin: "100px auto",
   };
 
-  return <svg class="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24"></svg>;
+  return (
+    <svg
+      className="block animate-spin h-10 w-10 mr-3 ..."
+      viewBox="0 0 24 24"
+    ></svg>
+  );
 };
 
 export default Spinner;
