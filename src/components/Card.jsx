52229c003bf481df396d16b429341a79faf14ad3
Electross <jarrettdh24@gmail.com>
2025-01-25 17:11:35 -0500

    Added favorite star for each research opportunity

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,25 @@
 import React from "react";
+import { useState } from "react";
+import star from "../assets/images/star.png";
+import star_filled from "../assets/images/star2.png";
 
-const Card = ({ research }) => {
+
+
+const Card = ({ research }) => { 
+  const [varStar, setVarStar] = useState(star);
+
+  function favoriteOpportunity()
+  {
+    if(varStar == "/src/assets/images/star.png")
+      setVarStar(star_filled);
+    else
+      setVarStar(star);
+  }
+
   return (
     <div className="fadeIn fadeOut w-full h-64 bg-white shadow-2xl rounded-xl p-10">
       <div className="flex flex-col">
-        <h3 className="font-bold text-xl">{research.name}</h3>
+       <div className="flex justify-between"> <h3 className="font-bold text-xl">{research.name}</h3> <button onClick={() => {favoriteOpportunity()}}><img className="relative w-9 h-9" src={varStar}/></button></div>
         <div>
           <h3>Topics: {research.topics?.join(" / ")}</h3>
         </div>
