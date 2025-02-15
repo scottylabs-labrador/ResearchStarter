37909f4dc3ef5e586f4d0f9e03489f9cdce92eea
Jennifer Zhu <jzhucs128@gmail.com>
2025-02-15 16:28:09 -0500

    added initial datatypes

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -4,12 +4,14 @@ import Card from "../components/Card";
 import { useState, useEffect } from "react";
 import Spinner from "../components/Spinner";
 
-import { useUser } from '@clerk/clerk-react'
+import { useUser } from "@clerk/clerk-react";
+import { UserResource, UseUserReturn } from "@clerk/types";
+import { ResearchType } from "./DataTypes";
 
 const DashboardPage = () => {
-  const [researches, setResearches] = useState([]);
+  const [researches, setResearches] = useState<ResearchType[]>([]);
   const [loading, setLoading] = useState(true);
-  const {user} = useUser();
+  const { user } = useUser();
 
   useEffect(() => {
     const fetchResearches = async () => {
@@ -17,7 +19,7 @@ const DashboardPage = () => {
         const res = await fetch("http://localhost:8000/research");
         const data = await res.json();
         setResearches(data);
-      } catch {
+      } catch (error) {
         console.log("Error Fetching Data", error);
       } finally {
         setLoading(false);
@@ -31,11 +33,11 @@ const DashboardPage = () => {
     <>
       <div>
         <h1 className="pt-20 ml-12 leading-loose text-6xl">
-        Welcome {user.firstName} {user.lastName}
+          Welcome {user?.firstName} {user?.lastName}
         </h1>
         <p className="h-32 leading-8 bg-gray-300 ml-12 mr-12"></p>
         <h1 className="ml-12 leading-normal text-6xl">
-        Featured Opportunities
+          Featured Opportunities
         </h1>
       </div>
 
@@ -49,7 +51,7 @@ const DashboardPage = () => {
           ) : (
             <>
               {researches.map((research) => (
-                <Card key={research.id} research={research}></Card>
+                <Card research={research}></Card>
               ))}
             </>
           )}
---
+++ src/pages/DataTypes.tsx
@@ -0,0 +1,23 @@
+export interface ResearchAllTypes {
+  research: ResearchType[];
+}
+
+export interface ResearchType {
+  id: string;
+  type: string;
+  name: string;
+  email: string;
+  officeHours: string;
+  topics: string[];
+  researches: string[];
+  keywords: string[];
+  colleges: string[];
+  department: string[];
+  labs: string[];
+  requestedYear: string;
+  requestedExp: string;
+  timeAvail: string;
+  pastPapers: string[];
+  description: string;
+  website: string;
+}
