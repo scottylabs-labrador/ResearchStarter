d39451e540b273a8f4d0a0f8eca5c18a10acd779
Jennifer Zhu <jzhucs128@gmail.com>
2025-02-22 17:45:20 -0500

    changed sign in css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  mode: 'jit',
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  mode: "jit",
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Monospace']
+        sans: ["Monospace"],
+        roboto: ["Roboto", "sans-serif"],
       },
       colors: {
-        'pink-hippo': '#fae0eb',
-        'magenta-hippo': '#ac316a',
-        'magenta-dark-hippo': '#4a152e',
-        'transparent-white': 'rgba(239, 239, 239, 0.7)',
-        'transparent-white-md': 'rgba(239, 239, 239, 0.9)',
+        "pink-hippo": "#fae0eb",
+        "magenta-hippo": "#ac316a",
+        "magenta-dark-hippo": "#4a152e",
+        "transparent-white": "rgba(239, 239, 239, 0.7)",
+        "transparent-white-md": "rgba(239, 239, 239, 0.9)",
+        "light-color": "#d9d9d9;",
+        "dark-color": "#b9b9b9",
+        "shadow-color": "#a8a8a8",
       },
       backgroundImage: {
-        'hippo-bg': "url(/src/assets/images/Hippo_Planet.png)"
-      }
-    }
+        "hippo-bg": "url(/src/assets/images/Hippo_Planet.png)",
+      },
+    },
   },
   plugins: [],
-}
+};
