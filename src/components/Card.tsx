54435bf50f9c5cfbea579aa4aca8bfb1887d69d3
Jennifer Zhu <jzhucs128@gmail.com>
2025-03-15 09:01:02 -0400

    fixed tags

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,6 +6,7 @@ import { NavLink } from "react-router-dom";
 import { ResearchType } from "~/DataTypes";
 import BookmarkIcon from "@mui/icons-material/Bookmark";
 import BookmarkIconUnfilled from "@mui/icons-material/BookmarkBorderOutlined";
+import Tag from "./Tag";
 
 interface CardPropt {
   research: ResearchType;
@@ -19,8 +20,8 @@ const Card = ({ research }: CardPropt) => {
   }
 
   return (
-    <div className="fadeIn fadeOut w-full h-64 bg-light-color rounded-xl p-10">
-      <div className="w-[75%] inline-block">
+    <div className="fadeIn fadeOut w-full h-72 bg-light-color rounded-xl p-10 relative flex items-start">
+      <div className="w-[73%] mr-[2%] inline-block">
         <h3 className="font-bold text-3xl mb-3">{research.name}</h3>{" "}
         {research.website != "" ? (
           <div>
@@ -43,13 +44,36 @@ const Card = ({ research }: CardPropt) => {
         </button>
       </div>
       <div className="w-[25%] inline-block">
-        <button
-          onClick={() => {
-            favoriteOpportunity();
-          }}
-        >
-          {varStar ? <BookmarkIcon /> : <BookmarkIconUnfilled />}
-        </button>
+        <div className="h-48 w-full overflow-hidden flex gap-y-2  gap-x-2 flex-wrap content-start items-start">
+          {research.colleges ? (
+            research.colleges.map((word) => (
+              <div>
+                <Tag keyword={word} />
+              </div>
+            ))
+          ) : (
+            <div className="collapse"></div>
+          )}
+          {research.department ? (
+            research.department.map((word) => <Tag keyword={word} />)
+          ) : (
+            <div className="collapse"></div>
+          )}
+          {research.keywords ? (
+            research.keywords.map((word) => <Tag keyword={word} />)
+          ) : (
+            <div className="collapse"></div>
+          )}
+        </div>
+        <div className="flex justify-end">
+          <button
+            onClick={() => {
+              favoriteOpportunity();
+            }}
+          >
+            {varStar ? <BookmarkIcon /> : <BookmarkIconUnfilled />}
+          </button>
+        </div>
       </div>
     </div>
   );
