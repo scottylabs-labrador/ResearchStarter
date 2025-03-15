5a7fa1f4cd4158331208eb1c2c722a9af267b57f
Jennifer Zhu <jzhucs128@gmail.com>
2025-03-15 09:54:46 -0400

    finished setting up saved page

---
+++ src/pages/SavedPage.tsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from "react";
+import SearchBar from "../components/SearchBar";
+import SavedCard from "../components/CardSaved";
+import Spinner from "../components/Spinner";
+import { ResearchType } from "../DataTypes";
+
+const SavedPage = () => {
+  const [researches, setResearches] = useState<ResearchType[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [input, setInput] = useState<string>("");
+
+  const handleChange = (value: string) => {
+    setInput(value);
+  };
+
+  let filteredData = researches;
+
+  useEffect(() => {
+    const fetchResearches = async () => {
+      try {
+        const res = await fetch("http://localhost:8000/research");
+        const data = await res.json();
+        setResearches(data);
+      } catch {
+        console.log("Error Fetching Data");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchResearches();
+  }, []);
+
+  return (
+    <div className="px-20 py-8">
+      <h1 className="text-3xl font-bold pb-10">Saved Research</h1>
+      <SearchBar input={input} handleChange={handleChange} />
+      <div className="w-full h-full grid grid-cols-1 items-stretch gap-5 mt-10">
+        {loading ? (
+          <>
+            <h2>Loading...</h2>
+            <Spinner />
+          </>
+        ) : (
+          <>
+            {filteredData.map((research) => (
+              <SavedCard key={research.id} research={research}></SavedCard>
+            ))}
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default SavedPage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -22,9 +22,10 @@ export default {
         "search-bar-color": "#C9C9C9",
         "filter-section-color": "#999898",
         "tag-dark-color": "#767676",
-        "bookmark-color": "##21272A",
+        "bookmark-color": "#21272A",
         "grey-blue-color": "#6D758F",
         "nav-border-color": "#DDE1E6",
+        "learn-more-color": "#c9c8c8",
       },
       backgroundImage: {
         "hippo-bg": "url(/src/assets/images/Hippo_Planet.png)",
