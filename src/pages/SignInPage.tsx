874b3d45ff7b6842530976bc06c9176254f291bb
Electross <jarrettdh24@gmail.com>
2025-12-06 01:22:54 -0500

    updated signin page design

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -5,13 +5,12 @@ import TartanLogo from "../assets/tartan_logo.png";
 
 const SignInPage = () => {
   return (
-    // Main container covering the full screen with a dark red background
+    // Main container covering the full screen
     <main className="absolute w-full h-full flex flex-col justify-center items-center bg-white">
-      {/* Large bold header at the top */}
       <img src={Logo} alt="Research Logo" className="h-40 mb-16" />
 
-      {/* White-centered section for the content */}
-      <section className="text-center flex flex-col justify-center items-center gap-y-10 p-12 md:p-20 border-transparent-white-md rounded-lg shadow-lg w-4/5 md:w-2/5 h-1/2 bg-light-color">
+      {/* Sign in section */}
+      <section className="text-center flex flex-col justify-center items-center gap-y-10 p-12 md:p-20 border-transparent-white-md shadow-lg w-3/4 h-3/5 md:w-1/3 bg-gradient-to-b from-violet-300 to-violet-100">
         <h2 className="text-3xl md:text-3xl font-medium leading-relaxed text-black font-roboto">
           <b>Sign in to your CMU Account</b>
         </h2>
@@ -23,7 +22,8 @@ const SignInPage = () => {
         />
 
         {/* Sign-in button styled to match the theme */} 
-        <button className="py-4 px-20 bg-dark-color shadow-md shadow-shadow-color text-white rounded-xl text-lg font-bold hover:bg-violet-600">
+        <button className="py-4 px-20 bg-brand-300 shadow-md shadow-shadow-color text-white rounded-xl text-lg font-bold 
+                         transition-all duration-300 ease-in-out hover:bg-brand-400 hover:scale-[1.05]">
           <SignInButton aria-label="Sign in to your CMU account" />
         </button>
       </section>
