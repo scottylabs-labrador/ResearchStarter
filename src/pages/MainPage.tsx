2b178c2524bea515be3a14744ea1b89efac991da
Jesse Chu <jesse.chu@outlook.com>
2025-12-16 08:56:20 -0800

    Updates on MainPage. Not finished yet.

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -5,30 +5,30 @@ import { NavLink } from "react-router-dom";
 const MainPage = () => {
   return (
     <div>
-      <div className="w-[100vw] h-[60vh] bg-light-color grid grid-cols-10 grid-rows-4">
+      <div className="w-[100vw] h-[60vh] bg-[linear-gradient(to_right,#E4D3FF,#C5A2FF)] grid grid-cols-10 grid-rows-4">
         <div className="col-start-1 col-end-4 grid-rows-1 flex items-center justify-center">
-          <h2 className="text-5xl font-semibold italic transition translate-x-5">
+          <h2 className="text-[64px] font-normal leading-[100%] tracking-[0%] font-jersey transition translate-x-5">
             Welcome to...
           </h2>
         </div>
         <div className="col-start-2 col-end-6 row-start-2 row-end-4 flex items-center">
-          <h1 className="text-9xl leading-tight tracking-wider font-bold animate-slidingIn">
+          <h1 className="text-[150px] font-normal leading-[100%] tracking-[0%] font-jersey animate-slidingIn">
             CMU Research
           </h1>
         </div>
         <div className="col-start-2 col-end-5 row-start-4 flex items-center">
           <div>
-            <button className="px-5 py-3 text-lg bg-tag-dark-color text-white rounded-xl mr-10">
+            <button className="w-[216px] h-[59px] px-3 py-2 text-[28px] font-normal leading-[100%] tracking-[0.5px] font-jersey-20 bg-tag-dark-color text-white rounded-[100px] mr-10">
               <NavLink to="/">Start Applying</NavLink>
             </button>
-            <button className="px-5 py-3 text-lg bg-transparent text-tag-dark-color rounded-xl font-bold border-[2px] border-tag-dark-color rounded">
+            <button className="w-[216px] h-[59px] px-3 py-2 text-[28px] font-normal leading-[100%] tracking-[0.5px] font-jersey bg-transparent text-tag-dark-color rounded-[100px] border-[4px] border-tag-dark-color">
               Start Posting
             </button>
           </div>
         </div>
       </div>
       <div className="w-[100vw] h-[50vh] px-14 py-16">
-        <h1 className="font-bold text-2xl">Selected Research Opportunities</h1>
+        <h1 className="text-[30px] font-normal leading-[110%] tracking-[0%] font-jersey align-middle">Selected Research Opportunities:</h1>
         <div></div>
       </div>
     </div>
